cc35d55a313f478470ff8d517b5c64b1f6e28001
Ingvard <ingvard777333@gmail.com>
2026-01-31 22:24:33 +0300

    run

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double height, double weight) {
+    public int calculate(double height, double weight) {
         double index = weight / (height * height);
         int BMI = (int) index;
         return BMI;
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double height = 1.87;
         double weight = 98;
-        double index = service.calculate(height, weight);
+        int index = service.calculate(height, weight);
 
         System.out.println("ваш рост " + height + " метра/ов");
         System.out.println("ваш вес " + weight + " килограмм");
